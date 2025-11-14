dc1426b2e6c6edd923a3848b1e43d786eea62e4f
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-14 13:09:04 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+/* Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno
+di una lista.
+ */
+/* Bonus
+
+Abbellire con CSS o Bootstrap
+Inserire un bottone che al click faccia il fetch altre 10 mail 
+(sostituendo quelle precedenti)
+Far sì che le email vengono stampati solo quando arrivano tutti i 
+10 email */
+
+console.log("ciao");
