e51ff606744507e0e0c8cf7cfc6123f52614288e
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-11-14 14:35:24 +0100

    feat: implemento funzione che genera email

--- javascript/script.js
+++ javascript/script.js
@@ -10,5 +10,22 @@ Inserire un bottone che al click faccia il fetch altre 10 mail
 (sostituendo quelle precedenti)
 Far sì che le email vengono stampati solo quando arrivano tutti i 
 10 email */
+const ElementUl = document.querySelector("ul");
+const btnElement = document.querySelector(".btn");
+const urlAPi = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-console.log("ciao");
+generaEmail();
+
+function generaEmail() {
+  const array = [];
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get(`${urlAPi}`)
+      .then((element) => {
+        const responseDati = element.data.response;
+        array.push(responseDati);
+        console.log(array);
+      })
+      .catch((errore) => {});
+  }
+}
